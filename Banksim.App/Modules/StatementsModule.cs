4d403f65502da2f43e02af11194eadd41494a7cd
a.mohandesi <a.mohandesi@A-MOHANDESI2-PR>
2025-09-14 16:49:03 +0330

    feat: add XML documentation and namespace declaration for StatementsModule

--- Banksim.App/Modules/StatementsModule.cs
+++ Banksim.App/Modules/StatementsModule.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
+namespace BankSim.App.Modules;
+
+/// <summary>
+/// This static class provides an extension method to register the Statements module services.
+/// </summary>
 public static class StatementsModule
 {
-    public static IServiceCollection AddStatementsModule(this IServiceCollection services) => services;
-
-    //TODO
+    /// <summary>
+    /// This method registers the services required for the Statements module.
+    /// </summary>
+    /// <param name="services">The service collection to which the module services will be added.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddStatementsModule(this IServiceCollection services)
+    {
+        // Register statement-related services here if needed in future
+        return services;
+    }
 }
