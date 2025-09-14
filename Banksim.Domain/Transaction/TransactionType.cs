dd202ccbef9a6ab53587cc78b2b0b247f5df9e8d
a.mohandesi <a.mohandesi@A-MOHANDESI2-PR>
2025-09-14 16:12:39 +0330

    feat: add TransactionType enum for different bank transaction types

---
+++ Banksim.Domain/Transaction/TransactionType.cs
@@ -0,0 +1,27 @@
+﻿namespace BankSim.Domain.Transaction.TransactionEnums;
+
+/// <summary>
+/// The types of transactions that can occur in a bank account.
+/// </summary>
+public enum TransactionType
+{
+    /// <summary>
+    /// The transaction is a deposit of funds into the account.
+    /// </summary>
+    Deposit,
+    
+    /// <summary>
+    /// The transaction is a withdrawal of funds from the account.
+    /// </summary>
+    Withdrawal,
+    
+    /// <summary>
+    /// The transaction is a transfer of funds into the account from another account.
+    /// </summary>
+    TransferIn,
+    
+    /// <summary>
+    /// The transaction is a transfer of funds out of the account to another account.
+    /// </summary>
+    TransferOut
+}
