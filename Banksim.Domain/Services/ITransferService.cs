db255ee7aabcee97b3e1c2661c8cc5b3e1a349f2
a.mohandesi <a.mohandesi@A-MOHANDESI2-PR>
2025-09-14 16:34:20 +0330

    feat: enhance ITransferService and TransferService with XML documentation and improved error handling for money transfers

--- Banksim.Domain/Services/ITransferService.cs
+++ Banksim.Domain/Services/ITransferService.cs
@@ -1,4 +1,19 @@
-﻿public interface ITransferService
+﻿using BankSim.Domain.Abstractions;
+using BankSim.Domain.ValueObjects;
+
+namespace BankSim.Domain.Services;
+
+/// <summary>
+/// Transfer service interface for handling money transfers between accounts.
+/// </summary>
+public interface ITransferService
 {
-    void Transfer(IAccount from, IAccount to, Money amount, string description= "" );
+    /// <summary>
+    /// The method to transfer money from one account to another.
+    /// </summary>
+    /// <param name="from">The account to transfer money from.</param>
+    /// <param name="to">The account to transfer money to.</param>
+    /// <param name="amount">The amount of money to transfer.</param>
+    /// <param name="description">An optional description for the transfer.</param>
+    void Transfer(IAccount from, IAccount to, Money amount, string description = "");
 }
