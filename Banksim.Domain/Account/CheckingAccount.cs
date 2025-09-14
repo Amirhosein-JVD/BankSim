eca8a29d2e85243462ee2b6b5fed6eee0c8e300c
a.mohandesi <a.mohandesi@A-MOHANDESI2-PR>
2025-09-14 16:26:08 +0330

    feat: add XML documentation and refactor withdrawal methods in CheckingAccount and SavingsAccount

--- Banksim.Domain/Account/CheckingAccount.cs
+++ Banksim.Domain/Account/CheckingAccount.cs
@@ -1,14 +1,29 @@
-﻿public class CheckingAccount : AccountBase
-{
-    public CheckingAccount(string owner, Money initialBalance) : base(owner, initialBalance) { }
+﻿using BankSim.Domain.Abstractions;
+using BankSim.Domain.ValueObjects;
 
-    public override void ProtectedWithdrawal(Money amount, TransactionType type, string description = "")
+namespace BankSim.Domain.Account;
+
+/// <summary>
+/// The CheckingAccount class represents a checking account in the banking system.
+/// It inherits from AccountBase and implements specific withdrawal rules.
+/// </summary>
+public class CheckingAccount : AccountBase
+{
+    /// <summary>
+    /// The constructor for the CheckingAccount class.
+    /// </summary>
+    /// <param name="owner">The owner of the account.</param>
+    /// <param name="initialBalance">The initial balance of the account.</param>
+    public CheckingAccount(string owner, Money initialBalance) : base(owner, initialBalance)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override void ProtectedWithdrawal(Money amount, string description = "")
     {
         if (Balance.Amount - amount.Amount < 100)
-        {
             throw new BusinessRuleViolationException();
-        }
+
         Balance = Balance.Subtract(amount);
-        _transactions.Add(new Transaction(amount, description, type));
     }
 }
