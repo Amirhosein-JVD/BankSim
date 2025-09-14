c53ca269ae5b111a8396c61cc52f85eee2fbc7b0
a.mohandesi <a.mohandesi@A-MOHANDESI2-PR>
2025-09-14 16:02:55 +0330

    feat: expand MoneyType enum to include additional currency types

--- Banksim.Domain/ValueObjects/MoneyEnums/MoneyType.cs
+++ Banksim.Domain/ValueObjects/MoneyEnums/MoneyType.cs
@@ -1,5 +1,17 @@
+namespace BankSim.Domain.ValueObjects.MoneyEnums;
+
+/// <summary>
+/// The enumeration of supported currency types.
+/// </summary>
 public enum MoneyType
 {
+    /// <summary>
+    /// The Iranian Rial currency.
+    /// </summary>
     IRR,
+    
+    /// <summary>
+    /// The US Dollar currency.
+    /// </summary>
     USD
 }
